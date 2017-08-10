c05b2e7fe4f78e118328160a5076b9aaa2a292e8
Igor Konyukhov <konuhovii@gmail.com>
2017-08-10 15:47:12 +0900

    Update KnifeBehavior.java

--- src/strategy/weapons/KnifeBehavior.java
+++ src/strategy/weapons/KnifeBehavior.java
@@ -3,6 +3,6 @@ package strategy.weapons;
 public class KnifeBehavior implements WeaponBehavior {
     @Override
     public void useWeapon() {
-        System.out.println("Хуяк ножом!!!");
+        System.out.println("Чик чик ножом!!!");
     }
 }
