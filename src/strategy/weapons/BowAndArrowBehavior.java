48e17c0b71bfa632c3b1f86a5561f1a70ebd5c10
Igor Konyukhov <konuhovii@gmail.com>
2017-08-10 15:46:55 +0900

    Update BowAndArrowBehavior.java

--- src/strategy/weapons/BowAndArrowBehavior.java
+++ src/strategy/weapons/BowAndArrowBehavior.java
@@ -3,6 +3,6 @@ package strategy.weapons;
 public class BowAndArrowBehavior implements WeaponBehavior {
     @Override
     public void useWeapon() {
-        System.out.println("Хуяк из лука в глаз!!!");
+        System.out.println("Чик из лука в глаз!!!");
     }
 }
