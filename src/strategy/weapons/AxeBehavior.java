b92376a14b5f523d4e4ea45750dad223b2423fac
Igor Konyukhov <konuhovii@gmail.com>
2017-08-10 15:46:21 +0900

    Update AxeBehavior.java

--- src/strategy/weapons/AxeBehavior.java
+++ src/strategy/weapons/AxeBehavior.java
@@ -3,6 +3,6 @@ package strategy.weapons;
 public class AxeBehavior implements WeaponBehavior {
     @Override
     public void useWeapon() {
-        System.out.println("Хуяк топором!!!");
+        System.out.println("Чик топором!!!");
     }
 }
